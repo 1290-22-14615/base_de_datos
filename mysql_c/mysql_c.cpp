c39c5978eb1b85b071bacf1a46e3556ff05e7cce
Marcos <f3rmarcos@gmail.com>
2023-04-25 22:52:08 -0600

    primer_commit

---
+++ mysql_c/mysql_c.cpp
@@ -0,0 +1,20 @@
+
+#include <mysql.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    MYSQL* conectar;
+    conectar = mysql_init(0);
+    conectar = mysql_real_connect(conectar, "localhost", "usr_empresa", "Empres@123", "db_empresa", 3306, NULL, 0);
+    if (conectar) {
+        cout << "conexion Exitosa..." << endl;
+    }
+    else {
+        cout << "error de conexion..." << endl;
+    }
+    system("pause");
+
+    return 0;
+}
+
